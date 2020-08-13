0da53d6a818782acb13165f2071d7f1ff242665f
Sofia Cuenca <cuens@farmingdale.edu>
2020-08-12 21:32:39 -0400

    image issue

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     content = models.TextField()
     createdOn = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    image = models.ImageField(default='default.png', upload_to='images/')
+    image = models.ImageField(upload_to='images/', blank=True)
 
 
     class Meta:
